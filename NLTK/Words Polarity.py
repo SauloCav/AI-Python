f15c5764910891ceab4d8edee47e2788125f41b5
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-02-11 18:06:48 -0300

    Add files via upload

---
+++ NLTK/Words Polarity.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+from textblob import TextBlob, Word, Blobber
+
+print("\nnegative_words.csv: ")
+file=open("negative_words.csv");
+t=file.read();
+text = TextBlob(t)
+print(text.sentiment)
+
+print('\n')
+
+print("positive_words.csv: ")
+file=open("positive_words.csv");
+t=file.read();
+text = TextBlob(t)
+print(text.sentiment)
